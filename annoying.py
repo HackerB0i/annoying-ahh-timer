e8da5e6f0663e778a6e812681a61744c08d5c63f
skonk <anntvfamily@gmail.com>
2024-03-18 09:10:02 -0700

    ffdas

--- annoying.py
+++ annoying.py
@@ -1,4 +1,19 @@
 import time
+from win10toast import ToastNotifier
+toast = ToastNotifier()
+# testing windows notifications
 
-duration = input("how long do you want to wait (seconds)")
+duration = int(input("how long do you want to wait (seconds): "))
+timestart = time.time()
 
+while time.time() - timestart < duration:
+    print(time.time() - timestart)
+
+while time.time() - timestart < duration:
+    toast.show_toast(
+    "timer is done",
+    f"your {duration} second timer is completed",
+    duration = 1,
+    icon_path = "icon.ico",
+    threaded = True,
+)
