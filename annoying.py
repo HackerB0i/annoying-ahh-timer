03565cee9b09557294e203caf89c4c8f54613839
skonk <anntvfamily@gmail.com>
2024-03-18 18:27:44 -0700

    notifications go forever now

--- annoying.py
+++ annoying.py
@@ -9,10 +9,10 @@ timestart = time.time()
 while time.time() - timestart < duration:
     print(time.time() - timestart)
 
-while time.time() - timestart < duration * 5:
+while True:
     toast.show_toast(
     "timer is done",
     f"your {duration} second timer is completed",
-    duration = 0,
+    duration = 1,
     threaded = True,
 )
